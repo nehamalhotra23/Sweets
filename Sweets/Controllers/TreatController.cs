95b92d7c5edb8279f07eda369993919a0466cd2b
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-10-25 10:28:02 -0700

    trying to add authorisation fot treats

--- Sweets/Controllers/TreatController.cs
+++ Sweets/Controllers/TreatController.cs
@@ -1,38 +1,87 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Sweets.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
 
 namespace Sweets.Controllers
 {
-  public class TreatController : Controller
-  {
-    private readonly SweetsContext _db;
-
-    public TreatController(SweetsContext db)
+    [Authorize]
+    public class TreatController : Controller
     {
-      _db = db;
-    }
+        private readonly SweetsContext _db;
+        private readonly UserManager<ApplicationUser> _userManager; 
 
-    public ActionResult Index()
-    {
-      List<Treat> model = _db.Treats.ToList();
-      return View(model);
-    }
 
-    public ActionResult Create()
-    {
-      return View();
-    }
+        public TreatController(UserManager<ApplicationUser> userManager, SweetsContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
 
+        public async Task<ActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            var userFlavours = _db.Flavours.Where(entry => entry.User.Id == currentUser.Id);
+            return View(userFlavours);
+        }
+
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
     [HttpPost]
-    public ActionResult Create(Treat treat)
-    {
-      _db.Treats.Add(treat);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+   [HttpPost]
+        public async Task<ActionResult> Create(Flavour flavour, string Treat1, string Treat2, string Treat3, string Treat4)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            flavour.User = currentUser;
+            _db.Flavours.Add(flavour);
+            _db.SaveChanges();
+            
+            List<string> treats = new List<string>();
+            treats.Add(Treat1);
+            treats.Add(Treat2);
+            treats.Add(Treat3);
+            treats.Add(Treat4);
+           
+            foreach(string treat in treats)
+            {
+                if (treat != null)
+                {
+                   Treat treatObject;
+                    int treatId;
+                    if(_db.Treats.Contains(new Treat() { TreatName = treat}))
+                    {
+                        treatObject = _db.Treats.FirstOrDefault(Treat => Treat.TreatName == treat);
+                        treatId = treatObject.TreatId;
+                    } 
+                    else 
+                    {
+                        var newTreat = new Treat();
+                        _db.Treats.Add(new Treat() { TreatName = treat}); // add treat to database
+                        _db.SaveChanges();
+                        treatObject = _db.Treats.FirstOrDefault(Treat => Treat.TreatName == treat);
+                        treatId = treatObject.TreatId;
+                    }
+                    _db.FlavourTreat.Add(new FlavourTreat() {TreatId = treatId, FlavourId = flavour.FlavourId});                    
+                }
+            }
+            
+
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     public ActionResult Details(int id)
     {
       List<FlavourTreat> flavourTreats = new List<FlavourTreat>();
